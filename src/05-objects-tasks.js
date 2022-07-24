1937c28ef1594113361a95ba545f562034ceac32
Tatiana Kostukevich <tankostuk@gmail.com>
2022-07-25 02:56:49 +0300

    feat: complete three tasks of objects

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,95 +20,101 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
 }
 
-
 /**
- * Returns the JSON representation of specified object
- *
- * @param {object} obj
- * @return {string}
- *
- * @example
- *    [1,2,3]   =>  '[1,2,3]'
- *    { width: 10, height : 20 } => '{"height":10,"width":20}'
- */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+   * Returns the JSON representation of specified object
+   *
+   * @param {object} obj
+   * @return {string}
+   *
+   * @example
+   *    [1,2,3]   =>  '[1,2,3]'
+   *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+   */
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
-
 /**
- * Returns the object of specified type from JSON representation
- *
- * @param {Object} proto
- * @param {string} json
- * @return {object}
- *
- * @example
- *    const r = fromJSON(Circle.prototype, '{"radius":10}');
- *
- */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+   * Returns the object of specified type from JSON representation
+   *
+   * @param {Object} proto
+   * @param {string} json
+   * @return {object}
+   *
+   * @example
+   *    const r = fromJSON(Circle.prototype, '{"radius":10}');
+   *
+   */
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
-
 /**
- * Css selectors builder
- *
- * Each complex selector can consists of type, id, class, attribute, pseudo-class
- * and pseudo-element selectors:
- *
- *    element#id.class[attr]:pseudoClass::pseudoElement
- *              \----/\----/\----------/
- *              Can be several occurrences
- *
- * All types of selectors can be combined using the combination ' ','+','~','>' .
- *
- * The task is to design a single class, independent classes or classes hierarchy
- * and implement the functionality to build the css selectors using the provided cssSelectorBuilder.
- * Each selector should have the stringify() method to output the string representation
- * according to css specification.
- *
- * Provided cssSelectorBuilder should be used as facade only to create your own classes,
- * for example the first method of cssSelectorBuilder can be like this:
- *   element: function(value) {
- *       return new MySuperBaseElementSelector(...)...
- *   },
- *
- * The design of class(es) is totally up to you, but try to make it as simple,
- * clear and readable as possible.
- *
- * @example
- *
- *  const builder = cssSelectorBuilder;
- *
- *  builder.id('main').class('container').class('editable').stringify()
- *    => '#main.container.editable'
- *
- *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
- *    => 'a[href$=".png"]:focus'
- *
- *  builder.combine(
- *      builder.element('div').id('main').class('container').class('draggable'),
- *      '+',
- *      builder.combine(
- *          builder.element('table').id('data'),
- *          '~',
- *           builder.combine(
- *               builder.element('tr').pseudoClass('nth-of-type(even)'),
- *               ' ',
- *               builder.element('td').pseudoClass('nth-of-type(even)')
- *           )
- *      )
- *  ).stringify()
- *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
- *
- *  For more examples see unit tests.
- */
+   * Css selectors builder
+   *
+   * Each complex selector can consists of type, id, class, attribute, pseudo-class
+   * and pseudo-element selectors:
+   *
+   *    element#id.class[attr]:pseudoClass::pseudoElement
+   *              \----/\----/\----------/
+   *              Can be several occurrences
+   *
+   * All types of selectors can be combined using the combination ' ','+','~','>' .
+   *
+   * The task is to design a single class, independent classes or classes hierarchy
+   * and implement the functionality to build the css selectors using the provided
+   * cssSelectorBuilder.
+   * Each selector should have the stringify() method to output the string representation
+   * according to css specification.
+   *
+   * Provided cssSelectorBuilder should be used as facade only to create your own classes,
+   * for example the first method of cssSelectorBuilder can be like this:
+   *   element: function(value) {
+   *       return new MySuperBaseElementSelector(...)...
+   *   },
+   *
+   * The design of class(es) is totally up to you, but try to make it as simple,
+   * clear and readable as possible.
+   *
+   * @example
+   *
+   *  const builder = cssSelectorBuilder;
+   *
+   *  builder.id('main').class('container').class('editable').stringify()
+   *    => '#main.container.editable'
+   *
+   *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()
+   *    => 'a[href$=".png"]:focus'
+   *
+   *  builder.combine(
+   *      builder.element('div').id('main').class('container').class('draggable'),
+   *      '+',
+   *      builder.combine(
+   *          builder.element('table').id('data'),
+   *          '~',
+   *           builder.combine(
+   *               builder.element('tr').pseudoClass('nth-of-type(even)'),
+   *               ' ',
+   *               builder.element('td').pseudoClass('nth-of-type(even)')
+   *           )
+   *      )
+   *  ).stringify()
+   *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
+   *
+   *  For more examples see unit tests.
+   */
 
 const cssSelectorBuilder = {
   element(/* value */) {
